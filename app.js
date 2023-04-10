55356ac335096fdf0ace4d0ad3eba7a6bbba96ad
ashtrindade <92398466+ashtrindade@users.noreply.github.com>
2023-04-10 20:12:24 -0300

    update ports

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const swaggerUi = require('swagger-ui-express')
 const routes = require('./src/routes/index')
 const swaggerDocument = require('./src/swagger/apiDocs.json')
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8080
 
 const app = express()
 
